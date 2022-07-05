5f0e33b70d755e11a87e9355b1034d7bc6f243b7
Marharyta Bosak <aboimova.rita@gmail.com>
2022-07-05 15:33:03 +0300

    Update Phonebook.jsx

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -5,30 +5,31 @@ import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filter/Filter';
 
-export default class Phonebook extends React.Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+export default function Phonebook() {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
+  const normalizedFilter = filter.toLowerCase();
+
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(normalizedFilter)
+  );
+
+  useEffect(() => {
     const contacts = localStorage.getItem('contacts');
     const parsedContacts = JSON.parse(contacts);
 
     if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
+      setContacts({ contacts: parsedContacts });
     }
-  }
+  }, []);
 
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts !== this.state.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-  formSubmitHandler = newContact => {
+  const formSubmitHandler = newContact => {
     const newStateContact = { id: uuidv4(), ...newContact };
-    const { contacts } = this.state;
 
     const existingContact = contacts.find(
       contact => contact.name === newContact.name
@@ -38,13 +39,13 @@ export default class Phonebook extends React.Component {
       return alert('This contact already exists');
     }
 
-    this.setState(prevState => {
+    setContacts(prevState => {
       return { contacts: [...prevState.contacts, newStateContact] };
     });
   };
 
-  deleteContactHandler = contactToDelete => {
-    this.setState(prevState => {
+  const deleteContactHandler = contactToDelete => {
+    setContacts(prevState => {
       const updatedContacts = prevState.contacts.filter(
         contact => contact !== contactToDelete
       );
@@ -55,30 +56,20 @@ export default class Phonebook extends React.Component {
     });
   };
 
-  filterHandler = e => {
-    this.setState({ filter: e.currentTarget.value });
+  const filterHandler = e => {
+    setFilter({ filter: e.currentTarget.value });
   };
 
-  render() {
-    const { contacts, filter } = this.state;
-
-    const normalizedFilter = filter.toLowerCase();
-
-    const filteredContacts = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter)
-    );
-
-    return (
-      <Container>
-        <h1>Phonebook</h1>
-        <ContactForm onSubmit={this.formSubmitHandler} />
-        <h2>Contacts</h2>
-        <Filter value={filter} onChange={this.filterHandler} />
-        <ContactList
-          contacts={filteredContacts}
-          onDelete={this.deleteContactHandler}
-        />
-      </Container>
-    );
-  }
+  return (
+    <Container>
+      <h1>Phonebook</h1>
+      <ContactForm onSubmit={formSubmitHandler} />
+      <h2>Contacts</h2>
+      <Filter value={filter} onChange={filterHandler} />
+      <ContactList
+        contacts={filteredContacts}
+        onDelete={deleteContactHandler}
+      />
+    </Container>
+  );
 }
